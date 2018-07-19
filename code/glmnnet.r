dd701b07ffae8f1374e9d2065a266c698eb8e428
Jared Lander <info@jaredlander.com>
2018-07-19 12:35:10 -0400

    Started glmnet

---
+++ code/glmnnet.r
@@ -0,0 +1,19 @@
+library(glmnet)
+library(useful)
+library(coefplot)
+library(magrittr)
+
+land_train <- readr::read_csv('data/manhattan_Train.csv')
+land_test <- readRDS('data/manhattan_Test.rds')
+
+View(land_train)
+
+valueFormula <- TotalValue ~ FireService + 
+    ZoneDist1 + ZoneDist2 + Class + LandUse + 
+    OwnerType + LotArea + BldgArea + ComArea + 
+    ResArea + OfficeArea + RetailArea + 
+    GarageArea + FactryArea + NumBldgs + 
+    NumFloors + UnitsRes + UnitsTotal + 
+    LotFront + LotDepth + BldgFront + 
+    BldgDepth + LotType + Landmark + BuiltFAR +
+    Built + HistoricDistrict - 1
